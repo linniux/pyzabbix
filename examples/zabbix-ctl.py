da9063d1d6b318ee89646ffda1234fbb50bd0927
linniux <zhiqian.wang@ali-90963n.hz.ali.com>
2015-03-06 18:21:10 +0800

    zabbix-ctl

--- examples/zabbix-ctl.py
+++ examples/zabbix-ctl.py
@@ -1,32 +1,68 @@
-
 from pyzabbix import ZabbixAPI,ZabbixAPIException
 
 import sys
+from _warnings import filters
 
-ZABBIX_SERVER = 'http://1.1.1.1'
+class AutoNaviZabbix(object):
+    """docstring for AutoNaviZabbix"""
 
-def main():
+    version = 0.1
+    
+    def __init__(self, zabbix_server,user,password):
+        super(AutoNaviZabbix, self).__init__()
+        
+        self.autonavi_zabbix_api = ZabbixAPI(zabbix_server)
+        self.autonavi_zabbix_api.login(user,password)
+     
 
-    autonavi_zabbix_api = ZabbixAPI(ZABBIX_SERVER)
-    autonavi_zabbix_api.login('asdf','asdf')
-    print "Connected to Zabbix API Version %s" % autonavi_zabbix_api.api_version()
+    def get_group_and_id(self):
+        """ return list """
+        hostgroup_objects = self.autonavi_zabbix_api.hostgroup.getobjects()
+        
+        return hostgroup_objects
+        
+    def list_host_by_groupid(self,groupid):
+        """ return list """
+        hosts = self.autonavi_zabbix_api.host.get(output=["hostid","host","status","available"],groupid=groupid)
+        
+        return hosts
+    
+    def enable(self,id):
 
-    host_name = 'aes'
-    #print autonavi_zabbix_api.host.get(filter={"host": host_name},selectInterfaces=["interfaceid"])
-    v = autonavi_zabbix_api.hostgroup.getobjects(groupids=["213"])
-    print v
-    sys.exit(3)
-    print type(v)
-    for i in v:
-        #print "hostgroup:  \033[31m%s\033[0m \tgroupid : %s" %(group['name'],group['groupid'])
-        print "hostgroup: \033[31m %s\033[0m \tgroupid: %s" % (i['name'],i['groupid'])
+        self.autonavi_zabbix_api.host.update(hostid=id,status=0)
+       
 
-def disable():
-    autonavi_zabbix_api = ZabbixAPI(ZABBIX_SERVER)
-    autonavi_zabbix_api.login('wangzhiqian','wangzhiqian')
-    print "Connected to Zabbix API Version %s" % autonavi_zabbix_api.api_version()
-    #autonavi_zabbix_api.hostgroup.update(groupid='213',)    
+    def disable(self,id):
+
+        self.autonavi_zabbix_api.host.update(hostid=id,status=1)
+
+
 
 
 if __name__ == '__main__':
-    main()
+    ZABBIX_SERVER = 'http://10.16.18.5'
+    t = AutoNaviZabbix(ZABBIX_SERVER,'wangzhiqian','wangzhiqian')
+    
+    
+    hosts = t.list_host_by_groupid('213')
+    status={"0":"OK","1":"Disabled"}
+    available={"0":"Unknown","1":"available","2":"Unavailable"}
+    
+    for i in hosts:
+        print "\\033[31m asdf "
+        print "HostID : %s\t HostName : %s\t Status :\033[32m%s\033[0m \t Available :\033[31m%s\033[0m"
+        print "hostid: \033[31m %s\033[0m \thost: %s\033[31m \tstatus: %s\033[31m \tavailable: %s\033[31m" % (i['hostid'],i['host'],status[i['status']],available[i['available']])
+        t.enable(i['hostid'])
+    
+    sys.exit(3)
+    
+#############    get group and id #############    
+    group_id = t.get_group_and_id()
+    
+    for i_dic in group_id:
+        #i_dic.pop('internal')
+        #i_dic.pop('flags')
+        print "hostgroup: \033[31m %s\033[0m \tgroupid: %s" % (i_dic['name'],i_dic['groupid'])
+        #for key in i_dic:
+        #   print key,"\t->\t",i_dic[key],'\n'
+   
